d0a3f30268df7b749d1d66ee540ad43d431f2aec
Marina8989 <marina.mill209@gmail.com>
2021-03-06 15:56:00 -0800

    hidden search widget

---
+++ main.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const button = document.querySelector('.btn');
+const search = document.querySelector('.search');
+
+button.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
